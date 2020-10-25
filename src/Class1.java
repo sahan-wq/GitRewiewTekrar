7925bfa0de6bd9c0122692e742fa02143f511e4a
KASIM <Ibrahim>
2020-10-25 21:50:30 +0100

    4. satir

--- src/Class1.java
+++ src/Class1.java
@@ -4,6 +4,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("saat 20:45");
 		System.out.println("saat 19:27-ibrahim");
+		System.out.println("Saat 19:24 - Hamza");
 		
 
 	}
